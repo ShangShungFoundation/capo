97996d65b693d5d7399a1b15904e0db1e77a06cd
Kamil <selwak@gmail.com>
2014-10-09 16:26:25 +0200

    remove propetary actions

--- capo/settings.py
+++ capo/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-ACTIONS = getattr(settings, "CAPO_ACTIONS", dict(
+ACTIONS = dict(
     cp="capo.actions.cp",
     exe="capo.actions.exe",
     send="capo.actions.email",
@@ -9,3 +9,7 @@ ACTIONS = getattr(settings, "CAPO_ACTIONS", dict(
     conv_to_mp3="capo.actions.exe",
     transfer_sh="capo.actions.transfer_sh",
 )
+
+CAPO_ACTIONS = getattr(settings, "CAPO_ACTIONS", {})
+
+ACTIONS.update(CAPO_ACTIONS)
