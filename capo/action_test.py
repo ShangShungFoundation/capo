b8b259433d9f90152ff4a585ae91315d749ec364
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-10 16:55:30 +0200

    add test for incorrectly configured task

--- capo/action_test.py
+++ capo/action_test.py
@@ -8,8 +8,6 @@ from settings import ACTIONS
 
 class ActionsTestCase(TestCase):
     def test_one_correctly_set_action_with_task_param(self):
-        a = ACTIONS
-
         task = Task
         task.action_name = "adding_dot"
         task.param = '{"parameter": "xxx"}'
@@ -20,3 +18,10 @@ class ActionsTestCase(TestCase):
             Recipe.validate_tasks_inputs(tasks)
         except Exception as e:
             self.fail("Unexpected exception raised: %s " % e)
+
+    def test_one_incorrectly_set_action_with_task_param(self):
+        task = Task
+        task.action_name = "adding_dot"
+        task.param = '{"parameter2": "xxx"}'
+        tasks = [task]
+        self.assertRaises(Exception, lambda: Recipe.validate_tasks_inputs(tasks))
