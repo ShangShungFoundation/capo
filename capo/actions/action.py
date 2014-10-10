b41638ae1c3fb9dedcd58a8949ce7e8c940ca7a5
Jakub Ryška <ryskajakub@seznam.cz>
2014-10-10 15:27:49 +0200

    delete trailing spaces

--- capo/actions/action.py
+++ capo/actions/action.py
@@ -3,17 +3,17 @@ class Action(object):
     expected_param = {}
     optional_param = {}
     expected_result = {}
-    
+
     result = {}
     def __init__(self, action_param = None):
         if action_param:
             self.action_param = action_param
             self.run(action_param)
-    
+
     def run(self, action_param):
         # this should be overwritten 
         pass
-    
+
     def error(self, msg):
         self.result["error"] = msg
         return False, self.result
